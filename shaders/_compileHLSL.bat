62fab43fde699fefbe169c3bb6684fe1cded656c
JasonNero <34405484+JasonNero@users.noreply.github.com>
2019-08-21 16:15:38 +0200

    added current SDK to HLSL compile and set debug flags
    https://devblogs.microsoft.com/pix/using-automatic-shader-pdb-resolution-in-pix/
    
    /Zi Turn on debug information.
    /Zss Include source when generating hash for the PDB name

--- shaders/_compileHLSL.bat
+++ shaders/_compileHLSL.bat
@@ -10,12 +10,14 @@ set FXC_PATH="C:\Program Files (x86)\Windows Kits\10\bin\10.0.16299.0\x64\fxc.ex
 if not exist !FXC_PATH! (
 set FXC_PATH="C:\Program Files (x86)\Windows Kits\10\bin\10.0.17134.0\x64\fxc.exe"
 if not exist !FXC_PATH! (
+set FXC_PATH="C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x64\fxc.exe"
+if not exist !FXC_PATH! (
 set FXC_PATH="C:\Program Files (x86)\Windows Kits\8.1\bin\x64\fxc.exe"
 if not exist !FXC_PATH! (
 echo fxc.exe not found on this computer
 pause
 exit
-)))))
+))))))
 if exist !FXC_PATH! (
 echo Effect compiler found
 echo %FXC_PATH%
@@ -30,7 +32,7 @@ for /f "tokens=1* delims=\" %%A in ('forfiles /s /m *.fx /c "cmd /c echo @relpat
 	REM echo %%~F
 	REM echo %%~nF
 	REM echo %%~pF
-	!FXC_PATH! /T fx_5_0 /Fo %%~Fo %%F
+	!FXC_PATH! /Zi /Zss /T fx_5_0 /Fo %%~Fo %%F
     )
 )
 echo All shaders seem to have been compiled
